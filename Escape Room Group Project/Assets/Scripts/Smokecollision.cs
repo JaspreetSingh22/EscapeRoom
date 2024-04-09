f528e6095b410977637fd885cd1cb3cfc9bfdca9
Jaspreet Singh <JaspreetSingh22@users.noreply.github.com>
2024-04-09 11:59:34 -0400

    FadeImproved

--- Escape Room Group Project/Assets/Scripts/Smokecollision.cs
+++ Escape Room Group Project/Assets/Scripts/Smokecollision.cs
@@ -21,27 +21,22 @@ public class Smokecollision : MonoBehaviour
             
             animator = FadeEffect.GetComponent<Animator>();
             FadeEffect.SetActive(true);
+            StartCoroutine("startTimer");
             Debug.Log("Damage FromSmoke");
         }
     }
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-          
-            StartCoroutine("startTimer");
-        }
-    }
+   
     IEnumerator startTimer()
     {
         
+       
+        yield return new WaitForSeconds(FadeTime);
         Player01.GetComponent<CharacterController>().enabled = false;
         Player02.GetComponent<CharacterController>().enabled = false;
         Player01.transform.position = Point_01.position;
         Player02.transform.position = Point_02.position;
         Player01.GetComponent<CharacterController>().enabled = true;
         Player02.GetComponent<CharacterController>().enabled = true;
-        yield return new WaitForSeconds(FadeTime);
         animator.SetTrigger("FadeOut");
         yield return new WaitForSeconds(1);
         FadeEffect.SetActive(false);
