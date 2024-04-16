5abc5bc767fe4a78571e03542216fd65bf4e727a
Jaspreet Singh <JaspreetSingh22@users.noreply.github.com>
2024-04-15 20:29:39 -0400

    f

--- Escape Room Group Project/Assets/Scripts/UImanager.cs
+++ Escape Room Group Project/Assets/Scripts/UImanager.cs
@@ -1,13 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UImanager : MonoBehaviour
 {
-    
+    Animator animator;
+    private void Start()
+    {
+        animator = GetComponent<Animator>();    
+    }
     public void onPlay()
     {
         Time.timeScale = 1.0f;
+        animator.SetTrigger("Move");
+    }
+    public void onHelp()
+    {
+        SceneManager.LoadScene("Help");
+    }
+    public void OnBack()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void AnimationDone()
+    {
         Destroy(gameObject);
     }
 }
